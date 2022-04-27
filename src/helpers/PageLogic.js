3c8efee32b09ff53c76da6312b3121b40ca151ca
mateotiedra <mateo@tiedra.com>
2022-04-27 11:22:33 +0200

    add navigate and actionIn /actionOut to pageLogic

--- src/helpers/PageLogic.js
+++ src/helpers/PageLogic.js
@@ -1,32 +1,46 @@
 import { useEffect, useRef, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import axios from 'axios';
 
 import AppConfig from '../config/AppConfig';
 import AxiosHelper from '../helpers/AxiosHelper';
 
-const PageLogic = (fetchData) => {
+const PageLogic = () => {
   const { API_ORIGIN } = AppConfig();
   const { getStatusCode } = AxiosHelper(axios);
+  let navigate = useNavigate();
 
   const [pageStatus, setPageStatus] = useState('loading');
   const hasFetchedData = useRef(false);
 
-  useEffect(() => {
+  /* useEffect(() => {
     if (!hasFetchedData.current) {
       hasFetchedData.current = true;
-      fetchData && fetchData();
+      loadPage && loadPage();
     }
 
     return () => {};
-  }, [fetchData]);
+  }, [loadPage]);*/
 
+  const useLoadPage = (actionIn, actionOut) => {
+    useEffect(() => {
+      if (!hasFetchedData.current) {
+        hasFetchedData.current = true;
+        actionIn && actionIn();
+      }
+
+      if (actionOut) return actionOut;
+    }, [actionIn, actionOut]);
+  };
   return {
     API_ORIGIN,
     getStatusCode,
     pageStatus,
     setPageStatus,
     axios,
+    navigate,
+    useLoadPage,
   };
 };
 
