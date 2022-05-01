a0b6dabff22995f4249eacb8fdfd4936ee713b6b
mateotiedra <mateo@tiedra.com>
2022-05-01 19:20:46 +0200

    add registration mode to auth page

---
+++ src/pages/Auth/Auth.js
@@ -0,0 +1,117 @@
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import LoadingButton from '@mui/lab/LoadingButton';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+
+import { FiUser } from 'react-icons/fi';
+
+import FormTextField from '../../components/FormTextField/FormTextField';
+import PasswordField from '../../components/PasswordField/PasswordField';
+import Navbar from '../../components/Navbar/Navbar';
+
+import AuthLogic from './AuthLogic';
+
+function Copyright(props) {
+  return (
+    <Typography
+      variant='body2'
+      color='text.secondary'
+      align='center'
+      {...props}
+    >
+      {'Copyright © '}
+      <Link color='inherit' href='https://mui.com/'>
+        CTT Bernex
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+export default function Auth({ startingMode }) {
+  const { register, errors, onSubmit, pageStatus, loginMode, switchLoginMode } =
+    AuthLogic({ startingMode: startingMode });
+
+  return (
+    <>
+      <Navbar coverPage empty />
+      <Container
+        component='main'
+        maxWidth='xs'
+        sx={{
+          height: '100vh',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+        }}
+      >
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1 }}>
+            <FiUser />
+          </Avatar>
+          <Typography component='h1' variant='h5'>
+            {loginMode ? 'Connexion' : 'Inscription'}
+          </Typography>
+          <Box component='form' onSubmit={onSubmit} noValidate sx={{ mt: 1 }}>
+            <FormTextField
+              required
+              id='email'
+              label='Adresse email'
+              autoComplete='email'
+              autoFocus
+              errors={errors}
+              registration={register('email', {
+                required: true,
+                pattern: /^[\w]+@([\w-]+\.)+[\w-]{2,4}$/g,
+              })}
+            />
+            <PasswordField
+              required
+              id='password'
+              label='Mot de passe'
+              errors={errors}
+              registration={register('password', {
+                required: true,
+              })}
+            />
+            <LoadingButton
+              variant='contained'
+              type='submit'
+              disableElevation
+              fullWidth
+              sx={{ mt: 3, mb: 2 }}
+              loading={pageStatus === 'sending'}
+            >
+              {loginMode ? 'Se connecter' : "S'inscrire"}
+            </LoadingButton>
+            <Grid container>
+              <Grid item xs>
+                <Link variant='body2' onClick={switchLoginMode}>
+                  {loginMode ? "S'inscrire" : 'Se connecter'}
+                </Link>
+              </Grid>
+              <Grid item md>
+                <Link href='#' variant='body2'>
+                  {loginMode && false && 'Mot de passe oublié ?'}
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+        <Copyright sx={{ mt: 8, mb: 4 }} />
+      </Container>
+    </>
+  );
+}
---
+++ src/pages/Auth/AuthLogic.js
@@ -0,0 +1,104 @@
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+
+import PageLogicHelper from '../../helpers/PageLogicHelper';
+
+const AuthLogic = ({ startingMode }) => {
+  const {
+    API_ORIGIN,
+    axios,
+    pageStatus,
+    setPageStatus,
+    getStatusCode,
+    navigate,
+    useLoadPage,
+  } = PageLogicHelper();
+
+  const {
+    register,
+    setError,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
+
+  const [loginMode, setLoginMode] = useState(startingMode === 'login');
+
+  const switchLoginMode = () => {
+    loginMode
+      ? navigate('/membre/inscription', {
+          replace: true,
+        })
+      : navigate('/membre/connexion', {
+          replace: true,
+        });
+
+    setLoginMode(!loginMode);
+  };
+
+  useLoadPage(() => {
+    axios
+      .get(API_ORIGIN + '/user/u', {
+        headers: {
+          'x-access-token': localStorage.getItem('accessToken'),
+        },
+      })
+      .then(({}) => {
+        navigate('/membre', { replace: true });
+      })
+      .catch((err) => {
+        if (getStatusCode(err) === 401) {
+          localStorage.removeItem('accessToken');
+        }
+      });
+  });
+
+  const onSubmit = (login) => (formData) => {
+    setPageStatus('sending');
+    axios
+      .post(API_ORIGIN + '/auth/' + (login ? 'signin' : 'signup'), {
+        email: formData.email,
+        password: formData.password,
+      })
+      .then(({ data }) => {
+        if (login) {
+          localStorage.setItem('accessToken', data.accessToken);
+          navigate('/membre', { replace: true });
+        } else {
+          onSubmit(true)(formData);
+        }
+      })
+      .catch((err) => {
+        if (login) {
+          if (getStatusCode(err) === 404) {
+            setError('email', { type: 'custom', message: 'Adresse inconnue' });
+          } else if (getStatusCode(err) === 403) {
+            setError('password', {
+              type: 'custom',
+              message: 'Mot de passe incorrect',
+            });
+          }
+        } else {
+          if (getStatusCode(err) === 409) {
+            setError('email', {
+              type: 'custom',
+              message: 'Adresse déjà utilisée par un autre compte',
+            });
+          } else {
+            console.log('fdsfdsafdsafdsafsda');
+          }
+        }
+        setPageStatus('active');
+      });
+  };
+
+  return {
+    pageStatus,
+    register,
+    errors,
+    onSubmit: handleSubmit(onSubmit(loginMode)),
+    loginMode,
+    switchLoginMode,
+  };
+};
+
+export default AuthLogic;
